a113b8f84199781ca17b4968d0302af3dfab9f4b
Lukas Hermann <lukher6@web.de>
2018-12-02 19:42:30 +0800

    Improve livestream styling

--- resources/templates/livestream.tpl.php
+++ resources/templates/livestream.tpl.php
@@ -20,9 +20,9 @@ use function Tonik\Theme\App\asset_path;
                 <h1><?php the_title() ?></h1>
             </header>
 
-            <div class="o-layout o-layout--large">
-                <div class="o-layout__item u-3/5@tablet u-2/3@desktop">
-                    <div class="o-ratio o-ratio--16:9 u-box-shadow ">
+            <div class="o-flex">
+                <div class="o-flex__item u-1/1 u-3/5@tablet u-2/3@desktop">
+                    <div class="o-ratio o-ratio--16:9 u-box-shadow u-1/1">
                         <iframe id="player"
                             class="o-ratio__content c-player"
                             src="<?= config('url-prefix')['embed'].'0livestream' ?>"
@@ -31,14 +31,12 @@ use function Tonik\Theme\App\asset_path;
                         </iframe>
                     </div>
                 </div>
-                <div class="o-layout__item u-2/5@tablet u-1/3@desktop u-mt u-mt0@tablet">
-                    <div class="u-anchor">
-                        <div class="c-tlkio">
+                <div class="o-flex__item u-1/1 u-2/5@tablet u-1/3@desktop u-mv u-m0@tablet u-anchor">
+                    <div class="c-tlkio">
 
-                            <!-- <div id="tlkio" data-channel="joelmedia"></div>
-                            <script async src="//tlk.io/embed.js" type="text/javascript"></script> -->
+                        <!-- <div id="tlkio" data-channel="joelmedia"></div>
+                        <script async src="//tlk.io/embed.js" type="text/javascript"></script> -->
 
-                        </div>
                     </div>
                 </div>
             </div>
@@ -47,36 +45,12 @@ use function Tonik\Theme\App\asset_path;
 
     <div class="o-wrapper u-mb++">
 
-        <div class="o-layout o-layout--large">
-            <div class="o-layout__item u-3/5@tablet u-2/3@desktop">
+        <div class="u-3/5@tablet u-2/3@desktop">
 
-                <div class="o-flex o-flex--middle o-flex--between u-pv">
-                    <div class="o-flex__item u-lead">
-                        <span class="c-primary-nav__signal"></span> <strong>Live</strong>
-                        <span class="u-hidden-from@desktop"><br>0:24:13</span>
-                    </div>
-                    <div class="o-flex__item u-text-center u-lead u-pl- u-hidden-until@desktop">
-                        0:24:13
-                    </div>
-                    <div class="o-flex__item u-pl@desktop">
-                        <div class="o-flag">
-                            <div class="o-flag__img">
-                                <img class="u-rounded u-hidden-until@desktop" src="{{image}}" width="80">
-                            </div>
-                            <div class="o-flag__body">
-                                <strong class="u-lead">Offenbarung Vers für Vers</strong>
-                                <br>Christopher Kramp · 19:30 – 20:15
-                            </div>
-                        </div>
-                    </div>
-                    <div class="o-flex__item u-text-right u-lead">
-                        <strong>20 <span class="u-hidden-until@tablet">Zuschauer</span></strong>
-                    </div>
-                </div>
+                <?php template('partials/livestream-meta') ?>
 
-                <hr/>
+                <hr class="u-m0" />
 
-            </div>
         </div>
 
         <div class="u-mt+">
---
+++ resources/templates/partials/livestream-meta.tpl.php
@@ -0,0 +1,27 @@
+<?php
+use function Tonik\Theme\App\config;
+?>
+
+<div class="o-flex o-flex--middle c-livestream-meta">
+    <div class="o-flex__item u-lead">
+        <span class="c-primary-nav__signal"></span> <strong>Live</strong>
+        <!-- <span class="u-hidden-from@desktop"><br>0:24:13</span> -->
+    </div>
+    <!-- <div class="o-flex__item u-text-center u-lead u-pl- u-hidden-until@desktop">
+        0:24:13
+    </div> -->
+    <div class="o-flex__item u-pl@desktop">
+        <div class="o-flag">
+            <div class="o-flag__img">
+                <img class="u-rounded u-hidden-until@desktop" src="{{image}}" width="80">
+            </div>
+            <div class="o-flag__body">
+                <strong class="u-lead">Offenbarung Vers für Vers</strong>
+                <br>Christopher Kramp · 19:30 – 20:15
+            </div>
+        </div>
+    </div>
+    <!-- <div class="o-flex__item u-text-right u-lead">
+        <strong>20 <span class="u-hidden-until@tablet">Zuschauer</span></strong>
+    </div> -->
+</div>
