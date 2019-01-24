f33b6effc09a0a17fda2d676d84baf25ccfb3512
Lukas Hermann <lukher6@web.de>
2019-01-24 14:26:20 +0800

    Add no-js placeholder for slider

--- resources/templates/vue-components/slider-init.tpl.php
+++ resources/templates/vue-components/slider-init.tpl.php
@@ -4,7 +4,7 @@ $options = isset($options) ? str_replace('"', "'", json_encode($options)) : '{}'
 ?>
 
 <!-- Vue slider root component -->
-<div v-cloak
+<div
     id="<?= $id ?>"
     class="<?= $style_modifier ?>"
     data-vue="slider"
@@ -17,6 +17,26 @@ $options = isset($options) ? str_replace('"', "'", json_encode($options)) : '{}'
         :slide-transition="slideTransition"
         :teaser="teaser"
     />
+
+    <!-- Placeholder -->
+    <div v-if="false" class="c-slider">
+        <ul class="c-slider__list">
+            <li class="c-slider__item">
+                <div class="c-slide c-slide-- c-slide--white" style="background-image: url(&quot;https://www.joelmedia.de/wordpress/wp-content/themes/joel/public/images/slide-bg1.png&quot;);">
+                    <a href="https://www.joelmedia.de/aufnahmen/" class="c-slide__link"></a>
+                    <div class="o-wrapper c-slide__wrapper">
+                        <div class="c-slide__body u-1/2">
+                            <img src="https://www.joelmedia.de/wordpress/wp-content/themes/joel/public/images/jm-logo-white-01.svg" class="c-logo c-logo--hero" alt="Joel Media">
+                            <h1 class="u-mb0 u-mt-">Joel Media Ministry e.V.</h1>
+                            <p class="u-muted"><small>... das ewige Evangelium für Stuttgart, Deutschland und die Welt</small></p>
+                            <p class="u-lead u-muted">Wöchentlich neue Videos über die Bibel,<br>Gesundheit und Zeitgeschehen</p>
+                            <a href="https://www.joelmedia.de/aufnahmen/" class="c-btn c-btn--dark c-slide__btn">Erster Besuch? Hier geht's los<span class="u-ic-arrow_forward"></span></a>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
 
 </div>
 
