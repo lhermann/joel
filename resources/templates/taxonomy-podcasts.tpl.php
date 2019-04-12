e3fd91f9e5fd14e86426d5c55ed2ff5590d0b4c4
Lukas Hermann <lukher6@web.de>
2019-04-12 09:04:45 +0200

    Update podcast xml

--- resources/templates/taxonomy-podcasts.tpl.php
+++ resources/templates/taxonomy-podcasts.tpl.php
@@ -7,9 +7,7 @@ use function Tonik\Theme\App\Legacy\trac_permalink;
 use function Tonik\Theme\App\Legacy\update_trac_database;
 use function Tonik\Theme\App\Legacy\get_video_file;
 
-
-header('Content-Type: application/rss+xml; charset=UTF-8');
-
+header( 'Content-Type: ' . feed_content_type( 'rss2' ) . '; charset=' . get_option( 'blog_charset' ), true );
 
 $term = get_queried_object();
 $fields = (object) get_fields($term);
@@ -20,16 +18,25 @@ update_trac_database('podcastping', $term->term_id, 'term');
 
 
 ?>
-<?php print('<?xml version="1.0" encoding="utf-8"?>'); //<? is interpreted as php short tag ?>
-<rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:media="http://search.yahoo.com/mrss/" version="2.0">
+<?php print('<?xml version="1.0" encoding="utf-8"?>'."\n"); //<? is interpreted as php short tag ?>
+<?php print('<?xml-stylesheet type="text/xsl" href="https://www.ideatodomundo.org/wp-content/plugins/seriously-simple-podcasting/templates/feed-stylesheet.xsl"?>'."\n"); ?>
+<rss version="2.0"
+    xmlns:atom="http://www.w3.org/2005/Atom"
+    xmlns:content="http://purl.org/rss/1.0/modules/content/"
+    xmlns:dc="http://purl.org/dc/elements/1.1/"
+    xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd"
+    xmlns:media="http://search.yahoo.com/mrss/"
+    xmlns:googleplay="http://www.google.com/schemas/play-podcasts/1.0"
+>
      <channel>
             <title><?php htmlspecialchars(single_term_title()) ?></title>
             <atom:link href="<?= get_term_link($term) ?>" rel="self" type="application/rss+xml" />
             <link><?= $link ?></link>
             <description><?= "<![CDATA[" . term_description() . "]]>" ?></description>
-            <lastBuildDate><?= get_the_date('r') ?></lastBuildDate>
+            <lastBuildDate><?= esc_html( mysql2date( 'D, d M Y H:i:s +0000', get_lastpostmodified( 'GMT' ), false ) ); ?></lastBuildDate>
             <language>de-DE</language>
             <copyright>Copyright &#xA9; Joel Media Ministry e.V. 2012-<?= date('Y') ?></copyright>
+            <webMaster>admin@joelmediatv.de (Lukas Hermann)</webMaster>
 
             <image>
                 <url><?= wp_get_attachment_url( get_field('image', $term)) ?></url>
@@ -37,11 +44,17 @@ update_trac_database('podcastping', $term->term_id, 'term');
                 <link><?= $link ?></link>
             </image>
 
+            <googleplay:author><?= $author ? $author.' / ' : '' ?>Joel Media Ministry e.V.</googleplay:author>
+            <googleplay:email>kontakt@joelmediatv.de</googleplay:email>
+            <googleplay:description><?= "<![CDATA[" . term_description() . "]]>" ?></googleplay:description>
+            <googleplay:explicit>No</googleplay:explicit>
+            <googleplay:image href="<?= wp_get_attachment_url( get_field('image', $term)) ?>"></googleplay:image>
+
             <itunes:subtitle>Ein Programm von Joel Media Ministry e.V.</itunes:subtitle>
             <itunes:author>Joel Media Ministry e.V.<?= $author ? ' / '.$author : '' ?></itunes:author>
             <itunes:summary><?= "<![CDATA[" . term_description() . "]]>" ?></itunes:summary>
             <itunes:owner>
-                    <itunes:name>Joel Media Ministry e.V.<?= $author ? ' / '.$author : '' ?></itunes:name>
+                    <itunes:name><?= $author ? $author.' / ' : '' ?>Joel Media Ministry e.V.</itunes:name>
                     <itunes:email>kontakt@joelmediatv.de</itunes:email>
             </itunes:owner>
             <itunes:image href="<?= wp_get_attachment_url( get_field('image', $term)) ?>"/>
@@ -53,6 +66,8 @@ update_trac_database('podcastping', $term->term_id, 'term');
             <media:category scheme="http://www.itunes.com/dtds/podcast-1.0.dtd">Christianity</media:category>
 
             <category>Religion &amp; Spirituality, Christianity</category>
+            <?= get_the_generator('rss2') ?>
+
             <?php
                 foreach ($fields->categorien as $i => $value) {
                     if( strpos($value, '/') ) {
