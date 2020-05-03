d7e8a2f19fec46c4e529fada114e435b9c0c78c5
Lukas Hermann <lukher6@web.de>
2020-05-03 09:38:31 +0200

    feat: enable streamcheck again

--- resources/assets/vue/livestream/streamcheck.vue
+++ resources/assets/vue/livestream/streamcheck.vue
@@ -9,24 +9,18 @@ import axios from "axios";
 
 export default {
   name: "Streamcheck",
-  props: {
-    stream: { type: String, default: "" }
-  },
   data() {
     return {
       loading: true,
       live: false,
     };
   },
-  mounted() {
-    this.loading = false;
-    // axios
-    //   .get(this.stream, {
-    //     baseURL: "//streamcheck.joelmedia.de/"
-    //   })
-    //   .then(response => (this.live = response.data.live))
-    //   .catch(error => console.log({ error }))
-    //   .then(() => (this.loading = false));
+  async mounted() {
+    axios
+      .get("//streamcheck.joelmedia.de/api/v1/status")
+      .then(({ data }) => (this.live = data.live))
+      .catch(error => console.log(error))
+      .then(() => (this.loading = false));
   }
 };
 </script>
