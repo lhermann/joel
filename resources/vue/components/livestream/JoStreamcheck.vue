36cb1612826e93023966face4c1c153506f801d3
Lukas Hermann <lukher6@web.de>
2020-05-22 10:25:58 +0200

    feat: update streamcheck every minute

--- resources/vue/components/livestream/JoStreamcheck.vue
+++ resources/vue/components/livestream/JoStreamcheck.vue
@@ -11,18 +11,28 @@ export default {
   name: 'Streamcheck',
   data () {
     return {
-      loading: true,
+      loading: false,
       live: false,
+      intervalID: null,
     }
   },
-  async mounted () {
-    try {
-      const { data } = await axios.get('//streamcheck.joelmedia.de/api/v1/status')
-      this.live = data.live
-    } catch (error) {
-      console.log(error) // eslint-disable-line
-    }
-    this.loading = false
+  created () {
+    this.intervalID = setInterval(this.getStatus, 60 * 1000)
+  },
+  beforeDestory () {
+    clearInterval(this.intervalID)
+  },
+  methods: {
+    async getStatus () {
+      this.loading = true
+      try {
+        const { data } = await axios.get('//streamcheck.joelmedia.de/api/v1/status')
+        this.live = data.live
+      } catch (error) {
+        console.log(error) // eslint-disable-line
+      }
+      this.loading = false
+    },
   },
 }
 </script>
